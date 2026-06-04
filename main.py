7f2a20ed54f3d51bef1a25dc864c44cac088d503
nxcuenca <nxcuenca@gmail.com>
2026-06-03 20:00:55 -0500

    Carga nueva

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = FastAPI()
 @app.get("/hello")
 def hola_mundo():
     return {"mesagge":"Hola Equipo"}
+
+@app.get("/")
+def root_endpoint():
+    return {"mesagge":"Bienvenido a mi API - LAB"}
