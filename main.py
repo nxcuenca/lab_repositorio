4f3217fc4e79408823cce88d3805b55131168c54
nxcuenca <nxcuenca@gmail.com>
2026-06-03 20:06:55 -0500

    Sumar

--- main.py
+++ main.py
@@ -9,3 +9,8 @@ def hola_mundo():
 @app.get("/")
 def root_endpoint():
     return {"mesagge":"Bienvenido a mi API - LAB"}
+
+@app.get("/suma")
+def sumar(a:int, b:int):
+    resultado = a + b
+    return {"resultado": resultado}
